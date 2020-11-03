46f4aded1b363231953de3a1b42b2a9310f4c373
Kingnaldosantos <62632450+Kingnaldosantos@users.noreply.github.com>
2020-11-03 18:06:56 -0300

    Add files via upload

---
+++ GCTM/js/script.js
@@ -0,0 +1,82 @@
+//botao de abrir
+var button_abrir1 = document.getElementById("button-ctm-net-abrir");
+    button_abrir1.addEventListener("click",function(){
+        var div_ctm_net =  document.getElementById("ctm-net");
+        if(div_ctm_net.style.display    === "none"){
+            div_ctm_net.style.display   =   "block";
+        }
+        else{
+            div_ctm_net.style.display = "none";
+        }
+    })
+var button_abrir2 = document.getElementById("button-ctm-tec-abrir");
+    button_abrir2.addEventListener("click",function(){
+        var div_ctm_tec =  document.getElementById("ctm-tec");
+        if(div_ctm_tec.style.display    === "none"){
+            div_ctm_tec.style.display   =   "block";
+        }
+        else{
+            div_ctm_tec.style.display = "none";
+        }
+    })
+var button_abrir3 = document.getElementById("button-ctm-msi-abrir");
+    button_abrir3.addEventListener("click",function(){
+        var div_ctm_msi =  document.getElementById("ctm-msi");
+        if(div_ctm_msi.style.display    === "none"){
+            div_ctm_msi.style.display   =   "block";
+        }
+        else{
+            div_ctm_msi.style.display = "none";
+        }
+    })
+var button_abrir4 = document.getElementById("button-ctm-faz-abrir");
+    button_abrir4.addEventListener("click",function(){
+        var div_ctm_faz =  document.getElementById("ctm-faz");
+        if(div_ctm_faz.style.display    === "none"){
+            div_ctm_faz.style.display   =   "block";
+        }
+        else{
+            div_ctm_faz.style.display = "none";
+        }
+    })
+//botoes de fechar
+var button_fechar1 =  document.getElementById("button-ctm-net-fechar");
+    button_fechar1.addEventListener("click",function(){
+        var div_ctm_net =  document.getElementById("ctm-net");
+        if(div_ctm_net.style.display    === "none"){
+            div_ctm_net.style.display   =   "block";
+        }
+        else{
+            div_ctm_net.style.display = "none";
+        }
+    })
+var button_fechar2 =  document.getElementById("button-ctm-tec-fechar");
+    button_fechar2.addEventListener("click",function(){
+        var div_ctm_tec =  document.getElementById("ctm-tec");
+        if(div_ctm_tec.style.display    === "none"){
+            div_ctm_tec.style.display   =   "block";
+        }
+        else{
+            div_ctm_tec.style.display = "none";
+        }
+    })
+var button_fechar3 =  document.getElementById("button-ctm-msi-fechar"); 
+    button_fechar3.addEventListener("click",function(){
+        var div_ctm_msi =  document.getElementById("ctm-msi");
+        if(div_ctm_msi.style.display    === "none"){
+            div_ctm_msi.style.display   =   "block";
+        }
+        else{
+            div_ctm_msi.style.display = "none";
+        }
+    })
+var button_fechar4 =  document.getElementById("button-ctm-faz-fechar"); 
+    button_fechar4.addEventListener("click",function(){
+        var div_ctm_faz =  document.getElementById("ctm-faz");
+        if(div_ctm_faz.style.display    === "none"){
+            div_ctm_faz.style.display   =   "block";
+        }
+        else{
+            div_ctm_faz.style.display = "none";
+        }
+    })
